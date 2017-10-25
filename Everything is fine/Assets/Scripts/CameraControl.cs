22c6a39d4680cdda6ea1f0869d8622447ab33155
Unknown <florian.carnac@gmail.com>
2017-10-25 11:05:15 +0200

    Fix du mouvement de camera + ajout de rotation de camera au clavier

--- Everything is fine/Assets/Scripts/CameraControl.cs
+++ Everything is fine/Assets/Scripts/CameraControl.cs
@@ -6,7 +6,8 @@ public class CameraControl : MonoBehaviour {
 
 	public GameObject camPosition;
 
-	public float speed = 2.0f;
+	public float moveSpeed = 2.0f;
+	public float rotateSpeed = 2.0f;
 
 	private float width;
 	private float height;
@@ -31,34 +32,56 @@ public class CameraControl : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+		//Camera movement
+		moveCam ();
+		rotateCam ();
+	}
+
+
+	void moveCam(){
 		Vector3 pos = Input.mousePosition;
 		Debug.Log (pos);
-		float newX = camPosition.transform.position.x;
-		float newZ = camPosition.transform.position.z;
+		float x_dir = 0;
+		float z_dir = 0;
 		bool changed = false;
 
 		if (pos.y < height5) {
-			newX += speed * Time.deltaTime;
+			z_dir -= moveSpeed * Time.deltaTime;
 			Debug.Log ("Reduction du X");
 			changed = true;
 		} else if (pos.y > height - height5) {
-			newX -= speed * Time.deltaTime;
+			z_dir += moveSpeed * Time.deltaTime;
 			Debug.Log ("Augmentation du X");
 			changed = true;
 		}
 
 		if (pos.x < width5) {
-			newZ -= speed * Time.deltaTime;
+			x_dir -= moveSpeed * Time.deltaTime;
 			Debug.Log ("Reduction du Z");
 			changed = true;
 		} else if (pos.x > width - width5) {
-			newZ += speed * Time.deltaTime;
+			x_dir += moveSpeed * Time.deltaTime;
 			Debug.Log ("Augmentation du Z");
 			changed = true;
 		}
 
-		if(changed)
-			camPosition.transform.position = new Vector3 (newX,camPosition.transform.position.y,newZ);
+		if (changed) {
+			Vector3 new_pos = camPosition.transform.localPosition + transform.TransformDirection(new Vector3 (x_dir, 0, z_dir));
+			camPosition.transform.localPosition = new_pos;
+		}
+	}
 
+	void rotateCam(){
+		Vector3 new_rot = camPosition.transform.rotation.eulerAngles;
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			new_rot.y += rotateSpeed * Time.deltaTime;
+		}
+
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			new_rot.y -= rotateSpeed * Time.deltaTime;
+		}
+
+		camPosition.transform.rotation = Quaternion.Euler(new_rot);
 	}
 }
